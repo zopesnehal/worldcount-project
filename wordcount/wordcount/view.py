74ae27f350a893d5dc23b1d7f161afd8f903348f
Snehal Zope <zopesnehal1@gamil.com>
2019-08-27 22:45:25 +0530

    added a template file

--- wordcount/wordcount/view.py
+++ wordcount/wordcount/view.py
@@ -1,12 +1,9 @@
 from django.http import HttpResponse
-
-
+from django.shortcuts import render
 
 
 def homepage (request):
-
-	return HttpResponse('hello')
+	return render(request, 'home.html')
 
 def eggs (request):
-
 	return HttpResponse('<h1>EGGS</h1>')  
